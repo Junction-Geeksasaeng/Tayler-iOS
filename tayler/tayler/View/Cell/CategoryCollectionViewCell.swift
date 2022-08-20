215462bcdb62e7565352f04b2cc2d6aaeec4ecb4
EunsuSeo01 <eunsu0613@naver.com>
2022-08-20 22:43:07 +0900

    Feat: 홈 화면 구성

---
+++ tayler/tayler/View/Cell/CategoryCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryCollectionViewCell.swift
+//  tayler
+//
+//  Created by 서은수 on 2022/08/20.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "CategoryCell"
+    
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var underlineView: UIView!
+}
