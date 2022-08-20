6dcbbbc1018201ed9779a052fe363c4e3ca831d6
EunsuSeo01 <eunsu0613@naver.com>
2022-08-20 17:12:35 +0900

    Chore: Project 생성

---
+++ tayler/tayler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tayler
+//
+//  Created by 서은수 on 2022/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
